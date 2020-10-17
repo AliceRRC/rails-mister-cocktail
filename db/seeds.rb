d91203c341a570ebbf475ff81a5b179677c35da9
Alice Ratton <aliceratton@gmail.com>
2020-10-17 17:04:23 -0300

    cocktail model controllers views  done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts "Cleaning database..."
+Cocktail.destroy_all
+
+puts "Creating cocktails..."
+mojito = { name: "Mojito" }
+gin_tonica =  { name: "Gin tonica" }
+
+[ mojito, gin_tonica ].each do |attributes|
+  cocktail = Cocktail.create!(attributes)
+  puts "Created #{cocktail.name}"
+end
+puts "Finished!"
